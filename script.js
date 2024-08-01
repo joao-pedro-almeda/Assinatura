85dcde0be2856dc2cde94278b1d40e31a5a1d8b6
joao-pedro-almeda <161710637+joao-pedro-almeda@users.noreply.github.com>
2024-08-01 08:06:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function carConteudo() {
+document.getElementById("imagem").innerHTML = "<img src='Captura de tela_FILMES-DRMA.png'>";
+
+}
+
+function carConteudo1() {
+
+    document.getElementById("imagem1").innerHTML = "<img src='realities.png'>";
+    
+    }
+
+    function carConteudo2() {
+
+        document.getElementById("imagem2").innerHTML = "<img src='realities.png'>";
+        
+        }
+
+        function carConteudo3() {
+
+            document.getElementById("imagem3").innerHTML = "<img src='realities.png'>";
+            
+            }
+
+            function carConteudo4() {
+
+                document.getElementById("imagem4").innerHTML = "<img src='realities.png'>";
+                
+                }
+
+                function carConteudo5() {
+
+                    document.getElementById("imagem5").innerHTML = "<img src='realities.png'>";
+                    
+                    }
