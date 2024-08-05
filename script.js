894cc9495bca1912859b2a96ed8008359c02cad8
joao-pedro-almeda <161710637+joao-pedro-almeda@users.noreply.github.com>
2024-08-05 10:53:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function carConteudo() {
-document.getElementById("imagem").innerHTML = "<img src='Captura de tela_FILMES-DRMA.png'>";
+
+    document.getElementById("imagem").innerHTML = "<img src='drama.png'>";
 
 }
 
@@ -11,24 +12,24 @@ function carConteudo1() {
 
     function carConteudo2() {
 
-        document.getElementById("imagem2").innerHTML = "<img src='realities.png'>";
+        document.getElementById("imagem2").innerHTML = "<img src='ação.png'>";
         
         }
 
         function carConteudo3() {
 
-            document.getElementById("imagem3").innerHTML = "<img src='realities.png'>";
+            document.getElementById("imagem3").innerHTML = "<img src='comedia.png'>";
             
             }
 
             function carConteudo4() {
 
-                document.getElementById("imagem4").innerHTML = "<img src='realities.png'>";
+                document.getElementById("imagem4").innerHTML = "<img src='policia.png'>";
                 
                 }
 
                 function carConteudo5() {
 
-                    document.getElementById("imagem5").innerHTML = "<img src='realities.png'>";
+                    document.getElementById("imagem5").innerHTML = "<img src='filmes.png'>";
                     
                     }
